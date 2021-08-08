21a2d9a192fac00c8d8bbf7a4dcfa2fa26c8bc88
naveed331 <naveed.ashfaq@graana.com>
2021-08-08 20:50:08 +0500

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = () => (
-    <div className='header'>
-      <Link className='logo-container' to='/'>
-        <Logo className='logo' />
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
       </Link>
-      <div className='options'>
-        <Link className='option' to='/shop'>
-          SHOP
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SiGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          signin
         </Link>
-        <Link className='option' to='/shop'>
-          CONTACT
-        </Link>
-      </div>
+      )}
     </div>
-  );
-  
+  </div>
+);
 
 export default Header;
